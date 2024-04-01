92d016317fdcae25fbc2f905d8fbf148a3bb3198
Oteri Eyenike <teyenike1@gmail.com>
2024-04-01 11:37:49 +0100

    Fix: change the title in the footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,9 @@
+"use client";
+
 export default function Footer() {
   return (
     <footer className='absolute bottom-0 py-1 text-center w-full'>
-      <p>2024 &copy; Teri</p>
+      <p>{new Date().getFullYear()} &copy; Track Trips</p>
     </footer>
   );
 }
