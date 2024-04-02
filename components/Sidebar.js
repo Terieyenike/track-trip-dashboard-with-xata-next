3e443afe00e845802b6485dbfdb4dc13bba10ea4
Oteri Eyenike <teyenike1@gmail.com>
2024-04-02 23:41:50 +0100

    Feat: implement the function to refresh the dashboard on click of a btn

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,6 +1,15 @@
+"use client";
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Sidebar() {
+  const router = useRouter();
+
+  const handleReload = () => {
+    router.refresh();
+  };
+
   return (
     <div className='md:h-screen bg-slate-700 text-white w-full md:w-2/5 md:max-w-xs lg:w-1/4 py-2 md:pt-8 md:pb-2 flex flex-col items-center justify-start border-r'>
       <div className='border-b w-full text-center pb-4 text-2xl'>
@@ -11,7 +20,9 @@ export default function Sidebar() {
       <div className='pt-4 w-full text-center'>
         <ul className='flex md:flex-col justify-around px-2 md:px-0 pb-2 md:pb-0 text-xs md:text-base lg:text-lg'>
           <Link href='/dashboard'>
-            <li className='py-4 px-3 md:px-0 hover:bg-gray-600 hover:cursor-pointer'>
+            <li
+              className='py-4 px-3 md:px-0 hover:bg-gray-600 hover:cursor-pointer'
+              onClick={handleReload}>
               My Trips
             </li>
           </Link>
