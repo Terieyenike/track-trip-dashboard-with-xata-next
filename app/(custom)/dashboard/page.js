40ce3b471bbf77af91f828af5411b0ad79dfe10d
Oteri Eyenike <teyenike1@gmail.com>
2024-04-22 00:36:59 +0100

    Feat: loading message depends on the state of the data

--- app/(custom)/dashboard/page.js
+++ app/(custom)/dashboard/page.js
@@ -9,14 +9,17 @@ export const revalidate = 0;
 
 export default function Dashboard() {
   const [trips, setTrips] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchTrips = async () => {
       try {
         const tripsData = await getTrips();
         setTrips(JSON.parse(tripsData));
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching trips:", error);
+        setLoading(false);
       }
     };
 
@@ -34,28 +37,35 @@ export default function Dashboard() {
   return (
     <>
       <Header name={"My Trips"} />
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        trips.length === 0 && <p>No travel memories found</p>
+      )}
 
-      {trips.length === 0 && <p>Loading...</p>}
-      {trips.map((trip) => (
-        <Link href={`dashboard/trip/${trip.id}`} key={trip.id}>
-          <div className='bg-gray-50 p-10 rounded shadow my-4 hover:shadow-lg hover:cursor-pointer'>
-            <p className='text-lg'>
-              {trip.country ? trip.country.toUpperCase() : ""} |{" "}
-              {trip.city
-                ? trip.city
-                    .toLowerCase()
-                    .split(" ")
-                    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-                    .join(" ")
-                : ""}
-            </p>
-            <p className='text-sm pt-4'>
-              <span>{trip.start ? formatDate(trip.start) : ""}</span> to{" "}
-              <span>{trip.end ? formatDate(trip.end) : ""}</span>
-            </p>
-          </div>
-        </Link>
-      ))}
+      {!loading &&
+        trips.map((trip) => (
+          <Link href={`dashboard/trip/${trip.id}`} key={trip.id}>
+            <div className='bg-gray-50 p-10 rounded shadow my-4 hover:shadow-lg hover:cursor-pointer'>
+              <p className='text-lg'>
+                {trip.country ? trip.country.toUpperCase() : ""} |{" "}
+                {trip.city
+                  ? trip.city
+                      .toLowerCase()
+                      .split(" ")
+                      .map(
+                        (word) => word.charAt(0).toUpperCase() + word.slice(1)
+                      )
+                      .join(" ")
+                  : ""}
+              </p>
+              <p className='text-sm pt-4'>
+                <span>{trip.start ? formatDate(trip.start) : ""}</span> to{" "}
+                <span>{trip.end ? formatDate(trip.end) : ""}</span>
+              </p>
+            </div>
+          </Link>
+        ))}
     </>
   );
 }
