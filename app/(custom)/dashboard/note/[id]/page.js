c996be1734f5abab94f37d0eb751b6ebf11a6240
Oteri Eyenike <teyenike1@gmail.com>
2024-05-02 10:57:02 +0100

    Feat: delete item with its unique id

--- app/(custom)/dashboard/note/[id]/page.js
+++ app/(custom)/dashboard/note/[id]/page.js
@@ -2,15 +2,44 @@ import Header from "@/components/Heading";
 import Link from "next/link";
 import Image from "next/image";
 import { capitalizeWords } from "@/utils/capitalizeWords";
-import generateDescriptionParagraphs from "@/utils/descriptionParagraphs";
 import { filter } from "@/utils/filter";
 import BtnDel from "@/components/BtnDel";
 
 export default async function NoteDetail({ params }) {
   const record = await filter(params);
 
-  const descriptionParagraphs = generateDescriptionParagraphs(
-    record.description
+  const noteInfo = (
+    <div className='bg-gray-50 p-8 rounded shadow hover:shadow-md my-4 md:flex xl:flex-row flex-col'>
+      <div className='xl:w-1/2 xl:flex-grow xl:mr-8'>
+        <h2 className='text-2xl font-semibold mb-4'>
+          {capitalizeWords(record.name)}
+        </h2>
+        <div className='mb-2'>
+          <span className='font-bold'>Description</span>:
+          {record.description.split("\n").map((paragraph, index) => (
+            <p key={index} className='mb-2'>
+              {paragraph}
+            </p>
+          ))}
+        </div>
+        <p className='mb-2'>
+          <span className='font-bold'>Type</span>: {record.type}
+        </p>
+        <p className='mb-2'>
+          <span className='font-bold'>Rating</span>: {record.rating}/5
+        </p>
+      </div>
+      <div className='xl:w-1/2 xl:ml-8'>
+        <Image
+          src={record.img.url}
+          priority={true}
+          width={300}
+          height={300}
+          alt={record.name}
+          className='w-full h-auto'
+        />
+      </div>
+    </div>
   );
 
   return (
@@ -22,40 +51,9 @@ export default async function NoteDetail({ params }) {
           className='px-7 py-2 bg-gray-300 rounded mr-4 hover:bg-gray-200'>
           Edit
         </Link>
-        <BtnDel delete={record.id} />
+        <BtnDel id={record.id} />
       </div>
-      {record ? (
-        <div className='bg-gray-50 p-8 rounded shadow hover:shadow-md my-4 md:flex xl:flex-row flex-col'>
-          <div className='xl:w-1/2 xl:flex-grow xl:mr-8'>
-            <h2 className='text-2xl font-semibold mb-4'>
-              {record.id}
-              {capitalizeWords(record.name)}
-            </h2>
-            <div className='mb-2'>
-              <span className='font-bold'>Description</span>:
-              {descriptionParagraphs}
-            </div>
-            <p className='mb-2'>
-              <span className='font-bold'>Type</span>: {record.type}
-            </p>
-            <p className='mb-2'>
-              <span className='font-bold'>Rating</span>: {record.rating}/5
-            </p>
-          </div>
-          <div className='xl:w-1/2 xl:ml-8'>
-            <Image
-              src={record.img.url}
-              priority={true}
-              width={300}
-              height={300}
-              alt={record.name}
-              className='w-full h-auto'
-            />
-          </div>
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
+      {noteInfo}
     </>
   );
 }
--- components/BtnDel.js
+++ components/BtnDel.js
@@ -1,7 +1,26 @@
-export default function BtnDel() {
+"use client";
+
+import { deleteNote } from "@/utils/delete";
+import { useRouter } from "next/navigation";
+
+export default function BtnDel({ id }) {
+  const router = useRouter();
+
+  const handleDelete = async () => {
+    try {
+      await deleteNote(id);
+      router.push("/dashboard/note/");
+    } catch (error) {
+      console.error("Error deleting note:", error);
+    }
+  };
   return (
-    <button className='px-7 py-2 bg-red-300 rounded hover:bg-red-200 hover:cursor-pointer'>
-      Delete
-    </button>
+    <>
+      <button
+        className='px-7 py-2 bg-red-300 rounded hover:bg-red-200 hover:cursor-pointer'
+        onClick={handleDelete}>
+        Delete
+      </button>
+    </>
   );
 }
