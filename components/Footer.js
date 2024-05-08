b0deca8b8358d5f9c0d6b8df6cab68c0d535d55f
Oteri Eyenike <teyenike1@gmail.com>
2024-05-08 14:56:59 +0100

    Update: footer text info

--- components/Footer.js
+++ components/Footer.js
@@ -2,9 +2,20 @@
 
 export default function Footer() {
   return (
-    <footer className='bg-gray-800 text-white py-8'>
+    <footer className='bg-gray-800 py-8'>
       <div className='container mx-auto px-5 text-center'>
-        <p>
+        <p className='text-gray-400 mb-4 text-sm'>
+          Designed and Developed by{" "}
+          <a
+            href='https://iamteri.tech'
+            target='_'
+            rel='noreferrer noopener'
+            className='text-white'>
+            Fitinth
+          </a>
+          .
+        </p>
+        <p className='text-xs text-gray-400'>
           &copy; {new Date().getFullYear()} Track Trips. All rights reserved.
         </p>
       </div>
